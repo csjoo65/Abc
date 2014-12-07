9ff72e21a1d5a3a330076fbb8fb11396026d2783
csjoo65 <csjoo65@naver.com>
2014-12-07 20:29:17 +0900

    2

--- sketch.js
+++ sketch.js
@@ -1,21 +1,7 @@
-function setup() {
-var bg;
-var y = 0;
+function setup(){
 
-function setup() {
- 
-  bg = loadImage("B1.png");
-  createCanvas(3333, 3333);
-}
 
-function draw() {
-  background(bg);
-  
-  stroke(226, 204, 0);
-  line(0, y, width, y);
-  
-  y++;
-  if (y > height) {
-    y = 0; 
-  }
-}
+   img = loadImage("B1_2.png");
+
+function draw(){
+   image(img,0,0);
