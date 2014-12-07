8b3ac9716230d9a2fd7f902cb6c45b05935b6096
csjoo65 <csjoo65@naver.com>
2014-12-07 22:07:35 +0900

    11

--- sketch.js
+++ sketch.js
@@ -1,6 +1,13 @@
 function setup(){
 
  	createCanvas(800, 800);
+
+
+ 	var img;
+ 	var B2_2
+ 	var R_2
+ 	var S_2
+
    img = loadImage("B1_2.png");
 	B2_2 = loadImage("B2_2.png");
 	R_2 = loadImage("R_2.png");
@@ -12,7 +19,7 @@ function draw(){
 
 // draw function
 if (mouseX < 300) {
-image(S_2,0,0);
+image(S_2,100,100,800,,800);
 } 
 else {
   ellipse(400, 200, 100, 100);
