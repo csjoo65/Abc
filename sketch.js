2d7661fc99a98c9dbb440e98575f60ee4da0f718
csjoo65 <csjoo65@naver.com>
2014-12-08 00:41:10 +0900

    8

--- sketch.js
+++ sketch.js
@@ -20,17 +20,17 @@ function setup(){
 
 function draw(){
 background(img);
-image(B2_2);
+
 
 if(mouseX<20){
 image(S_2,0,3,800,800);;
 }
-else if (mouseX>10){
+else if (mouseX>800){
 image(R_2,0,3,800,800);
 }
 else{
 ellipse(400,200,0,0);
 }
 
-
+image(B2_2);
 }
